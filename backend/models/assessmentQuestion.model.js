0dbf174ee9ed009a88557a7f190279d63fa26973
Kk-0010 <krishnakantyadav853@gmail.com>
2025-02-06 19:56:05 +0530

    assessmentQuestions

---
+++ backend/models/assessmentQuestion.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const optionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const assessmentQuestionSchema = new mongoose.Schema(
+  {
+    questionText: {
+      type: String,
+      required: true,
+    },
+
+    stream: {
+      type: String,
+      enum: ["Science", "Commerce", "Arts", "Common"],
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: ["objective", "subjective"],
+      required: true,
+    },
+
+    objectiveAnswers: {
+      type: [optionSchema],
+      required: function () {
+        return this.type === "objective";
+      },
+      default: [],
+    },
+
+    subjectiveAnswer: {
+      type: String,
+      trim: true,
+      required: function () {
+        return this.type === "subjective";
+      },
+    },
+  },
+  { timestamps: true }
+);
