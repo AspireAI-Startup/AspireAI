5fe81f5c121b5790a8ddcef5e9e176238c664d10
Harshita Gupta <harshita.g212@gmail.com>
2025-02-06 17:33:26 +0530

    user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,44 +1,51 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.model (
-    {
-        fullName:{
-            firstName:{
-                type:String,
-                required:true
-            },
-            lastName:{
-                type:String,
-                required:true
-            }
-        },
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
 
-        email:{
-            type:String,
-            required:true,
-            unique:true
-        },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-        password:{
-            type:String,
-            required:true
-        },
+    password: {
+      type: String,
+      required: true,
+    },
 
-        mobileNo:{
-            type:Number,
-            required:true,
-            unique:true
-        },
+    mobileNo: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
 
-        academicLevel:{
-            type:String,
-            enum:["class 10", "class 12"],
-            required:true
-        },
-        
-
-},{timestamps:true}
+    academicLevel: {
+      type: String,
+      enum: ["class 10", "class 12"],
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
+userSchema.statics.hashPassword = async function (password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
 
-export const User = mongoose.model("User",userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
